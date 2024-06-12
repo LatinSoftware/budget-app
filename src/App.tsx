b09912cd2464d8988056ae251fc704acc8e909e4
Caonabo Mena Perez <camena@banreservas.com>
2024-06-12 19:14:08 -0400

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import './App.css'
-import { Button } from '@mui/material';
+import './App.css';
+import Root from './routes/root';
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: 'dark',
+    },
+  });
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Root />,
+    },
+  ]);
+
   return (
-    <>
-      <h1>Welcome to your budget app</h1>
-      <Button variant="contained">Hello world</Button>;
-    </>
-  )
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
